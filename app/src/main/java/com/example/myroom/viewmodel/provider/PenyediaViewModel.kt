b0651c787dc6cc9c5c3f993134a6ee08f4552832
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 18:57:10 +0700

    tambah function creatin extras

--- app/src/main/java/com/example/myroom/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroom/viewmodel/provider/PenyediaViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.myroom.viewmodel.provider
 
+import androidx.app.Aplication
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroom.repositori.AplikasiSiswa
 import com.example.myroom.viewmodel.EntryViewModel
 import com.example.myroom.viewmodel.HomeViewModel
 
@@ -24,3 +27,6 @@ object PenyediaViewModel{
     instance dari
  * [AplikasiSiswa].
  */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiSiswa)
