bdeab4dea042d31c326a18a924b70c93c79ad529
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 16:43:09 +0700

    tambah class

--- app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
@@ -1,6 +1,16 @@
 package com.example.myroom.repositori
 
+import android.content.Context
+import com.example.myroom.room.DatabaseSiswa
+
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSsiswa(
+            siswaDaoDatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
