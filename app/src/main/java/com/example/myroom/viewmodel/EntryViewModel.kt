0928d59a97f925948e402f5aa0cba5daf852a63b
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 17:09:35 +0700

    tambah data class detail siswa

--- app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.myroom.viewmodel
 
 import com.example.myroom.room.Siswa
-
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
 fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
