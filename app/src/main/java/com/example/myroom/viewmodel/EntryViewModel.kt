a7f94ca6cda6886656e4150c6ec4d89e7a6f58b1
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 17:07:12 +0700

    tambah function siswa to ui state dan to detail

---
+++ app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.myroom.viewmodel
+
+import com.example.myroom.room.Siswa
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
