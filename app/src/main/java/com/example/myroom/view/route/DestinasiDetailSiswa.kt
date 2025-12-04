25db356080548bd02a6303a11b8560dc9a47c2ac
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-04 15:12:43 +0700

    Menambahkan DestinasiDetailSiswa

---
+++ app/src/main/java/com/example/myroom/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,8 @@
+package com.example.myroom.view.route
+
+import com.example.myroom.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+}
